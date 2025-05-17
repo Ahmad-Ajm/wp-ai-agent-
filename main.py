916ddc32825ee0ad7332a611503f3c08b071ad95
Ahmad-Ajm <ahaj82@hotmail.com>
2025-05-17 06:46:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://your-wordpress-domain.com"],
+    allow_origins=["https://shop112.rf.gd/"],
     allow_credentials=True,
     allow_methods=["POST", "GET", "OPTIONS"],
     allow_headers=["*"],
